ae6f56fe8de390dd13b26c402266698198abdb9d
chengcong <chengcong.mjm@autonavi.com>
2023-04-17 17:08:58 +0800

    flyweight pattern

--- 13_flyweight/02_pool/app.go
+++ 13_flyweight/02_pool/app.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Conn struct {
-	iddr string
+	addr string
 	port int64
 }
 
@@ -22,16 +22,16 @@ func init() {
 
 func App() {
 	conn1 := connPool.Get().(*Conn)
-	fmt.Printf("&conn1=%p, iddr=%s port=%d\n", conn1, conn1.iddr, conn1.port)
+	fmt.Printf("&conn1=%p, addr=%s port=%d\n", conn1, conn1.addr, conn1.port)
 	connPool.Put(conn1)
 
 	conn2 := connPool.Get().(*Conn)
-	fmt.Printf("&conn2=%p, iddr=%s port=%d\n", conn2, conn2.iddr, conn2.port)
+	fmt.Printf("&conn2=%p, addr=%s port=%d\n", conn2, conn2.addr, conn2.port)
 
 	fmt.Println("conn1==conn2", conn1 == conn2)
 
 	conn3 := connPool.Get().(*Conn)
-	fmt.Printf("&conn3=%p, iddr=%s port=%d\n", conn3, conn3.iddr, conn3.port)
+	fmt.Printf("&conn3=%p, addr=%s port=%d\n", conn3, conn3.addr, conn3.port)
 
 	fmt.Println("conn2==conn3", conn2 == conn3)
 }
