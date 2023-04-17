e9f703d7ce238cabe994f96a693fd566d575398a
chengcong <chengcong.mjm@autonavi.com>
2023-04-17 20:54:44 +0800

    prototype pattern via gob

--- 06_prototype/02_glob/app.go
+++ 06_prototype/02_glob/app.go
@@ -44,7 +44,5 @@ func App() {
 	act2 := NewAct("新活动", "20-10-10", &act1)
 	fmt.Printf("act1 name=%s coupon.name=%s coupon.rule=%s\n", act1.Name, act1.CouponTpl.Name, act1.CouponTpl.Rule)
 	fmt.Printf("act2 name=%s coupon.name=%s coupon.rule=%s\n", act2.Name, act2.CouponTpl.Name, act2.CouponTpl.Rule)
-
 	fmt.Println(act1.CouponTpl.Rule == act2.CouponTpl.Rule)
-
 }
