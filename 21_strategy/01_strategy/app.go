57216848a53d048f6a365c5a3106e378453b6833
chengcong <chengcong.mjm@autonavi.com>
2023-04-19 13:18:02 +0800

    Strategy pattern

--- 21_strategy/01_strategy/app.go
+++ 21_strategy/01_strategy/app.go
@@ -3,28 +3,39 @@ package strategy
 import "fmt"
 
 type Notifyer interface {
+	FetchIdentify(uid int64) string
 	Send(msg string) bool
 }
 
 var _ Notifyer = (*Push)(nil)
 
 type Push struct {
-	deviceId string
+	uid int64
+}
+
+func (p *Push) FetchIdentify(uid int64) string {
+	return fmt.Sprintf("%d-ios-14-10010", p.uid)
 }
 
 func (p *Push) Send(msg string) bool {
-	fmt.Printf("%s Send push %s\n", p.deviceId, msg)
+	deviceId := p.FetchIdentify(p.uid)
+	fmt.Printf("%s Send push %s\n", deviceId, msg)
 	return true
 }
 
 var _ Notifyer = (*SMS)(nil)
 
 type SMS struct {
-	phone string
+	uid int64
+}
+
+func (s *SMS) FetchIdentify(uid int64) string {
+	return "18519191001"
 }
 
 func (s *SMS) Send(msg string) bool {
-	fmt.Printf("%s Send sms %s\n", s.phone, msg)
+	phone := s.FetchIdentify(s.uid)
+	fmt.Printf("%s Send sms %s\n", phone, msg)
 	return true
 }
 
@@ -41,16 +52,12 @@ func (nc *NotifyCtx) Send(msg string) bool {
 }
 
 func Client() {
-	push := &Push{
-		"ios-14-10010",
-	}
+	push := &Push{110110}
 	notifyCtx := NotifyCtx{}
 	notifyCtx.SetNotifyer(push)
 	notifyCtx.Send("hello world")
 
-	sms := &SMS{
-		"18519191001",
-	}
+	sms := &SMS{110110}
 	notifyCtx.SetNotifyer(sms)
 	notifyCtx.Send("hello world")
 }
